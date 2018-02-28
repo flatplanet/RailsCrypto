8381ad048599e801feedb52a910ce499def5db3c
John Elder <johne4196@gmail.com>
2018-02-28 16:55:42 +0000

    Added Devise

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'devise', '~> 4.4', '>= 4.4.1'
+
 
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/about'
+  get 'home/lookup'
+  post "/home/lookup" => 'home/lookup'
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
